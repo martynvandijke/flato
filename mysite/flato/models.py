fa5975a7817c2150a5d242862bd2eefd4208c576
Martyn van Dijke <martijn_vdijke@hotmail.com>
2018-01-14 23:15:39 +0100

    niel working version very buggy

--- mysite/flato/models.py
+++ mysite/flato/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -75,3 +77,19 @@ class Movie(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)  # set the slug explicitly
         super(Movie, self).save(*args, **kwargs)  # call Django's save()
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
